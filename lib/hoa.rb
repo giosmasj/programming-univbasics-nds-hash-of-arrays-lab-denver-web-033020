8843122ec1d4f6cb214a8e0f601bc2bb29b73f3b
James Giosmas <giosmasj@gmail.com>
2020-03-14 03:51:34 +0000

    Automatically backed up by Learn

--- lib/hoa.rb
+++ lib/hoa.rb
@@ -9,6 +9,5 @@ def add_character(show, name)
   # Write your implementation here
   # Should return the array of the 'show' argument
   add_character[:show][i] = name
-end
-return add_character
+add_character[show]
 end
